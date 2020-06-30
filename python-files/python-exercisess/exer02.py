a5b0386c55fd27c6f46be9a4188d0f9ca7939dbe
Luan <luanmateuslima34@gmail.com>
2020-06-30 20:15:35 -0300

    Uploading some exercises

---
+++ python-files/python-exercisess/exer02.py
@@ -0,0 +1,3 @@
+# Entre com um nome e apresente um mensagem
+nome = input('Digite seu nome: ')
+print('É um prazer em te conhecer, {}!'.format(nome))
---
+++ python-files/python-exercisess/exer03-somando_dois_valores.py
@@ -0,0 +1,7 @@
+print('===========================\n')
+print('<-- Soma entre números -->\n')
+print('===========================\n')
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+s = n1 + n2
+print('A soma entre {} e {} será: {}'.format(n1, n2, s))
